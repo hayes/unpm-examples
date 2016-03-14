3f54f3594cd51b9479c9fda4885b00f2a30c30ce
Michael Hayes <mhayes@newrelic.com>
2016-03-14 01:09:13 -0700

    wip

---
+++ s3-ldap-www/index.js
@@ -0,0 +1,61 @@
+var dependents = require('unpm-dependents')
+var backend = require('unpm-s3-backend')
+var ldapAuth = require('unpm-ldap')
+var www = require('unpm-www')
+var UNPM = require('unpm')
+var path = require('path')
+
+var bucket = process.env.UNPM_S3_BUCKET
+
+var config = {
+  host: {
+    hostname: process.env.UNPM_HOST || 'localhost',
+    port: process.env.UNPM_PORT || 8123,
+    protocol: 'http'
+  },
+  backend: backend({
+    s3: {params: {Bucket: bucket}},
+    baseUrl: 'https://s3-us-east-1.amazonaws.com/' + bucket + '/',
+    users: {prefix: '~/users/'), Bucket: bucket},
+    meta: {prefix: '/', Bucket: bucket},
+    store: {prefix: '~/store/'), Bucket: bucket},
+    tarballs: {prefix: '/'), Bucket: bucket}
+  }),
+  User: ldapAuth({
+    url: 'ldap://localhost:8321',
+    searchBase: 'ou=example-org',
+    searchFilter: '(uid={{username}})'
+  })
+}
+
+var unpm = new UNPM(config)
+
+dependents(unpm)
+www(null, '', 'unpm')
+unpm.middleware.push(docs)
+unpm.middleware.push(status)
+unpm.server.listen(unpm.port)
+unpm.log.info('Started unpm on port %s', unpm.port)
+
+function docs(respond, matched, unpm, next) {
+  var headers = respond.req.headers
+  var route
+
+  var acceptable = headers.accept && headers.accept.match(/image|(text\/(?:html|css))/)
+  var tarball = respond.req.url.match(/\.tgz$/i)
+
+  if (acceptable && !tarball) {
+    route = www.router.match(respond.req.method, respond.req.url)
+  }
+
+  if (!route) {
+    return next()
+  }
+
+  route.fn(respond.req, respond.res, route, www.config)
+}
+
+function status(respond, matched, unpm, next) {
+  if (respond.req.url !== '/status/check') return next()
+  respond(200, {status: 'ok'})
+}
